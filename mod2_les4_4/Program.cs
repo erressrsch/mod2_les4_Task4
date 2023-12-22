bc7b83022b3285ff7dba390574ae9ce703658a53
Szymon Chyc <erressrsch@gmail.com>
2023-12-22 21:20:38 +0100

    Add project files.

---
+++ mod2_les4_4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace mod2_les4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            string name;
+            double b;
+
+            a = 10;
+            name = "Szkoła Dotneta";
+            b = 12.5;
+
+            Console.WriteLine(a);
+            Console.WriteLine(name);
+            Console.WriteLine(b);
+
+
+        }
+    }
+
+
+}
